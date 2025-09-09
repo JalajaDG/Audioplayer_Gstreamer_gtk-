cc036f47843719f58afc755913de4debcaaf5856
JalajaDG <jalajagond@gmail.com>
2025-09-09 13:03:03 +0530

    Add GTK dialog for detailed audio metadata: title, artist, album, year, duration (min:sec), comment, container, codec, channels, sample rate

--- metadata.cpp
+++ metadata.cpp
@@ -3,39 +3,28 @@
 #include <gst/gst.h>
 #include <gtk/gtk.h>
 #include <iostream>
-#include <gst/pbutils/pbutils.h>   // <-- for GstDiscoverer
-
+#include <gst/pbutils/pbutils.h>
 
 using namespace std;
 
-// Callback for "Metadata" button click
 void on_metadata_clicked(GtkWidget* widget, gpointer data) {
     GtkWidget* window = GTK_WIDGET(data);
 
-    // Get the singleton player
+    // Get singleton player
     PlayAudio* player = PlayAudio::getInstance();
-    if (!player) {
-        g_printerr("Error: PlayAudio instance is NULL\n");
-        return;
-    }
+    if (!player) return;
 
-    // Ensure pipeline exists
     GstElement* pipeline = player->getPipeline();
-    if (!pipeline) {
-        g_printerr("Error: pipeline is NULL\n");
-        return;
-    }
+    if (!pipeline) return;
 
-    // Get current song filepath
     if (song_list.empty() || currently_playing_song_index >= (int)song_list.size()) {
-        g_printerr("No song is currently selected\n");
+        g_printerr("No song selected\n");
         return;
     }
 
     string filepath = folder_Path + "/" + song_list[currently_playing_song_index];
     g_print("Getting metadata for: %s\n", filepath.c_str());
 
-    // Create GstDiscoverer
     GError* err = nullptr;
     GstDiscoverer* discoverer = gst_discoverer_new(5 * GST_SECOND, &err);
     if (!discoverer) {
@@ -52,37 +41,93 @@ void on_metadata_clicked(GtkWidget* widget, gpointer data) {
         return;
     }
 
+    // Extract tags
     const GstTagList* tags = gst_discoverer_info_get_tags(info);
-    gchar* title = nullptr;
-    gchar* artist = nullptr;
-    gchar* album = nullptr;
-
+    gchar *title = nullptr, *artist = nullptr, *album = nullptr, *comment = nullptr, *year_str = nullptr;
     if (tags) {
         gst_tag_list_get_string(tags, GST_TAG_TITLE, &title);
         gst_tag_list_get_string(tags, GST_TAG_ARTIST, &artist);
         gst_tag_list_get_string(tags, GST_TAG_ALBUM, &album);
+        gst_tag_list_get_string(tags, GST_TAG_COMMENT, &comment);
+        gst_tag_list_get_string(tags, GST_TAG_DATE, &year_str);
     }
 
-    // Prepare metadata string
-    gchar metadata_text[1024];
+    // Duration in minutes and seconds
+    GstClockTime duration = gst_discoverer_info_get_duration(info);
+    guint total_sec = (guint)(duration / GST_SECOND);
+    guint duration_min = total_sec / 60;
+    guint duration_sec = total_sec % 60;
+
+    // Container and audio info
+    gchar *container = nullptr;
+    gchar *codec = nullptr;
+    guint channels = 0;
+    guint sample_rate = 0;
+
+    const GstDiscovererStreamInfo* sinfo_const = gst_discoverer_info_get_stream_info(info);
+    if (sinfo_const) {
+        GstDiscovererStreamInfo* sinfo = (GstDiscovererStreamInfo*)sinfo_const;
+        GstCaps* caps = gst_discoverer_stream_info_get_caps(sinfo);
+
+        if (caps && gst_caps_get_size(caps) > 0) {
+            GstStructure* s = gst_caps_get_structure(caps, 0);
+            container = g_strdup(gst_structure_get_name(s));
+
+            gchar* caps_str = gst_caps_to_string(caps);
+            if (g_str_has_prefix(caps_str, "audio/")) {
+                codec = g_strdup(gst_structure_get_name(s));
+                gint c = 0, rate = 0;
+                gst_structure_get_int(s, "channels", &c);
+                gst_structure_get_int(s, "rate", &rate);
+                channels = c;
+                sample_rate = rate;
+            }
+            g_free(caps_str);
+        }
+    }
+
+    // Prepare formatted metadata with spacing and tabs
+    gchar metadata_text[2048];
     snprintf(metadata_text, sizeof(metadata_text),
-             "Title : %s\nArtist: %s\nAlbum : %s",
+             "<b>General</b>\n"
+             "  Title      : %s\n"
+             "  Artist     : %s\n"
+             "  Album      : %s\n"
+             "  Year       : %s\n"
+             "  Duration   : %u min %u sec\n"
+             "  Comment    : %s\n"
+             "  Container  : %s\n\n"
+             "<b>Audio</b>\n"
+             "  Codec      : %s\n"
+             "  Channels   : %u\n"
+             "  Sample rate: %u Hz",
              title ? title : "Unknown",
              artist ? artist : "Unknown",
-             album ? album : "Unknown");
+             album ? album : "Unknown",
+             year_str ? year_str : "Unknown",
+             duration_min,
+             duration_sec,
+             comment ? comment : "Unknown",
+             container ? container : "Unknown",
+             codec ? codec : "Unknown",
+             channels,
+             sample_rate
+    );
 
-    // Print to console (optional)
-    g_print("%s\n", metadata_text);
+    // Create GTK dialog with markup
+    GtkWidget* dialog = gtk_message_dialog_new(GTK_WINDOW(window),
+                                               static_cast<GtkDialogFlags>(GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT),
+                                               GTK_MESSAGE_INFO,
+                                               GTK_BUTTONS_OK,
+                                               NULL);
 
-    // Show GTK dialog
-  GtkWidget* dialog = gtk_message_dialog_new(
-    GTK_WINDOW(window),
-    static_cast<GtkDialogFlags>(GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT),
-    GTK_MESSAGE_INFO,
-    GTK_BUTTONS_OK,
-    "%s",
-    metadata_text
-);
+    GtkWidget* label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), metadata_text);
+    gtk_label_set_xalign(GTK_LABEL(label), 0); // left align
+
+    GtkWidget* content_area = gtk_message_dialog_get_message_area(GTK_MESSAGE_DIALOG(dialog));
+    gtk_container_add(GTK_CONTAINER(content_area), label);
+    gtk_widget_show_all(dialog);
 
     gtk_dialog_run(GTK_DIALOG(dialog));
     gtk_widget_destroy(dialog);
@@ -91,6 +136,10 @@ void on_metadata_clicked(GtkWidget* widget, gpointer data) {
     g_free(title);
     g_free(artist);
     g_free(album);
+    g_free(comment);
+    g_free(year_str);
+    g_free(container);
+    g_free(codec);
     g_object_unref(info);
     g_object_unref(discoverer);
 }
